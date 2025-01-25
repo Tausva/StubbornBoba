3efc0ac70fd339bcc2f6253b501618337ff9c3bd
Tausva <tautvydas.svarinskas@gmail.com>
2025-01-25 13:07:53 +0200

    two cups and one girl

--- Scenes/Cup/cup.gd
+++ Scenes/Cup/cup.gd
@@ -2,17 +2,20 @@ class_name Cup extends Area2D
 
 signal empty
 
-@onready var tea_collision_shape: CollisionShape2D = $Tea/CollisionShape2D
+@onready var tea_collision_shape: CollisionShape2D = $Tea/CollisionShape
+@onready var tea_sprite: Sprite2D = $Tea/CupShapeSprite/TeaSprite
+@onready var cup_shape_sprite: Sprite2D = $Tea/CupShapeSprite
 
 var fill_percentage: float = 1.0
 var original_tea_height: float
 var original_tea_position: Vector2
+var original_tea_sprite_scale: float
 
 
 func _ready() -> void:
 	original_tea_height = (tea_collision_shape.shape as RectangleShape2D).size.y
 	original_tea_position = tea_collision_shape.position
-	print(str(original_tea_height) + " " + str(original_tea_position))
+	original_tea_sprite_scale = tea_sprite.scale.y
 
 
 func _process(delta: float) -> void: # testing, delet
@@ -34,3 +37,8 @@ func drain(percentage: float) -> void:
 func _update_play_area() -> void:
 	(tea_collision_shape.shape as RectangleShape2D).size.y = original_tea_height * fill_percentage
 	tea_collision_shape.position.y = original_tea_position.y * fill_percentage
+	
+	var position_to_go = cup_shape_sprite.position.y / cup_shape_sprite.scale.y * -1
+	tea_sprite.scale.y = original_tea_sprite_scale * fill_percentage
+	tea_sprite.position.y = position_to_go - position_to_go * fill_percentage
+	
