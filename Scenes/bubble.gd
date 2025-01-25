0c204b6be1d47dbb21958dc6337f05dccf8c74e2
Martynas Gedutis <martynasgedutis@gmail.com>
2025-01-25 21:38:38 +0200

    bublel sizuka 1.0

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -6,16 +6,19 @@ class_name Bubble extends CharacterBody2D
 @export var dash_speed: int = 100000
 @export var dash_duration: float = 0.2
 @export var dash_cooldown: float = 5.0
+@export var jump_cooldown: float = 5.0
+@export var collision_shape_water: CollisionShape2D 
 
 var is_dashing: bool = false
 var dash_direction: Vector2 = Vector2.ZERO
 var dash_timer: float = 0.0 
-var cooldown_timer: float = 0.0
+var dash_cooldown_timer: float = 0.0
+var jump_cooldown_timer: float = 0.0
 var can_jump: bool = false
 var disable_vertical_movement: bool = false
 var jump_activated = false
-var bubble_exited_Area_count = 0;
 var is_above_water = false
+var collision_shape_water_count: int = 0
 
 func _ready() -> void:
 	dash_particles.emitting = false
@@ -35,49 +38,40 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 
-	if disable_vertical_movement and direction.y > 0:
-		velocity = Vector2.ZERO
-	print(disable_vertical_movement)
-	if disable_vertical_movement and bubble_exited_Area_count == 1:
-		velocity.y += -1000
-		#can_jump = false
-		is_above_water = true
-	
-	if is_above_water and bubble_exited_Area_count == 1:
-		velocity.y += speed * delta
-		
-		#if disable_vertical_movement:
-			#velocity = Vector2.ZERO
-		#else:
-			#velocity = direction.normalized() * speed * delta
-	
-	#if direction.y > 0 and disable_vertical_movement == true:
-		#if jump_activated:
-			#print("bomba")
-			#velocity.y += speed * delta
-		#else:
-			#velocity = Vector2.ZERO
-	#elif direction != Vector2.ZERO:
-		#print("1")
-		#velocity = direction.normalized() * speed * delta
-	#else:
+	#if disable_vertical_movement and direction.y > 0:
 		#velocity = Vector2.ZERO
 
-	if cooldown_timer > 0:
-		cooldown_timer -= delta
-		if cooldown_timer < 0:
-			cooldown_timer = 0
+	if disable_vertical_movement and direction.y < 0:
+		velocity.y += -1000
+		disable_vertical_movement = true
+
+	if is_above_water and jump_activated:
+		velocity.y += speed * delta
+
+	if dash_cooldown_timer > 0:
+		dash_cooldown_timer -= delta
+		if dash_cooldown_timer < 0:
+			dash_cooldown_timer = 0
 	if is_dashing:
 		dash_timer -= delta
 
 		if dash_timer <= 0:
 			is_dashing = false
-			cooldown_timer = dash_cooldown
+			dash_cooldown_timer = dash_cooldown
 			dash_particles.emitting = false
 		else:
 			velocity += dash_direction * dash_speed * delta
-	
-	if Input.is_action_just_pressed("bubble_dash") and cooldown_timer <= 0 and direction != Vector2.ZERO:
+
+	if jump_cooldown_timer > 0:
+		jump_cooldown_timer -= delta
+		if jump_cooldown_timer < 0:
+			jump_cooldown_timer = 0
+
+	if jump_cooldown_timer <= 0:
+		can_jump = true
+		#call_deferred("collision_shape_water_enabled")
+
+	if Input.is_action_just_pressed("bubble_dash") and dash_cooldown_timer <= 0 and direction != Vector2.ZERO:
 		_start_dash(direction)
 	move_and_slide()
 
@@ -96,18 +90,40 @@ func _start_dash(direction: Vector2) -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	can_jump = true
 	disable_vertical_movement = true
 
 	if can_jump:
+		print("ar tu ateini")
+		call_deferred("collision_shape_water_disabled")
+		can_jump = false
 		jump_activated = true
+		is_above_water = true
+		jump_cooldown_timer = jump_cooldown
+	else: 
+		jump_activated = false
+
+	if jump_cooldown_timer <= 0:
+		call_deferred("collision_shape_water_enabled")
+		is_above_water = false
 	pass # Replace with function body.
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	bubble_exited_Area_count += 1
-	print(bubble_exited_Area_count)
-	if bubble_exited_Area_count == 2:
-		print("bingo2")
-		disable_vertical_movement = false
+	collision_shape_water_count += 1
+	print(collision_shape_water_count)
+	if collision_shape_water_count == 2 :
+		call_deferred("collision_shape_water_enabled")
+		collision_shape_water_count == 0
 	pass # Replace with function body.
+
+
+func collision_shape_water_disabled() -> void:
+		collision_shape_water.disabled = true
+
+
+func collision_shape_water_enabled() -> void:
+	collision_shape_water.disabled = false
+
+
+#func collision_shape_exited() -> void:
+	#collision_shape_water.area
